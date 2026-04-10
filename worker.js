e4e818341f9ec5deda9adccdbe91409f8d67e4f7
starcherk <kylestarcher@rogers.com>
2026-04-10 07:29:44 -0400

    test push1

--- worker.js
+++ worker.js
@@ -124,7 +124,7 @@ function portalHTML() {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>JamJunction &mdash; Access</title>
+  <title>JamJunction &mdash; Access - test</title>
   <style>
     *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
     :root {
